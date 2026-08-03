bb49775c05ac1ac0bf99143b3d2f0bb562dbcd11
kiyasu <1207514710@qq.com>
2026-08-03 17:39:11 +0800

    feat: 2-5 装配域节点-AiApiNode

--- ai-agent-scaffold-domain/src/main/java/com/kiyasu/ai/domain/agent/service/armory/node/AiApiNode.java
+++ ai-agent-scaffold-domain/src/main/java/com/kiyasu/ai/domain/agent/service/armory/node/AiApiNode.java
@@ -2,10 +2,13 @@ package com.kiyasu.ai.domain.agent.service.armory.node;
 
 import cn.bugstack.wrench.design.framework.tree.StrategyHandler;
 import com.kiyasu.ai.domain.agent.model.entity.ArmoryCommandEntity;
+import com.kiyasu.ai.domain.agent.model.valobj.AiAgentConfigTableVO;
 import com.kiyasu.ai.domain.agent.model.valobj.AiAgentRegisterVO;
 import com.kiyasu.ai.domain.agent.service.armory.AbstractArmorySupport;
 import com.kiyasu.ai.domain.agent.service.armory.factory.DefaultArmoryFactory;
+import com.networknt.schema.utils.StringUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.ai.openai.api.OpenAiApi;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -14,9 +17,20 @@ public class AiApiNode extends AbstractArmorySupport {
     @Override
     protected AiAgentRegisterVO doApply(ArmoryCommandEntity requestParameter, DefaultArmoryFactory.DynamicContext dynamicContext) throws Exception {
 
-        //todo 编写api实例化操作
+        log.info("Ai Agent 装配操作 - AiApiNode");
+        AiAgentConfigTableVO aiAgentConfigTableVO = requestParameter.getAiAgentConfigTableVO();
+        AiAgentConfigTableVO.Module.AiApi aiApiConfig = aiAgentConfigTableVO.getModule().getAiApi();
 
+        OpenAiApi openAiApi = OpenAiApi.builder()
+                .baseUrl(aiApiConfig.getBaseUrl())
+                .apiKey(aiApiConfig.getApiKey())
+                .completionsPath(StringUtils.isNotBlank(aiApiConfig.getCompletionsPath()) ? aiApiConfig.getCompletionsPath() : "v1/completions")
+                .embeddingsPath(StringUtils.isNotBlank(aiApiConfig.getEmbeddingsPath()) ? aiApiConfig.getEmbeddingsPath() : "v1/embeddings")
+                .build();
 
+        dynamicContext.setOpenAiApi(openAiApi);
+
+        //路由到下个节点，如果不需要路由则return返回结果
         return router(requestParameter, dynamicContext);
     }
 
