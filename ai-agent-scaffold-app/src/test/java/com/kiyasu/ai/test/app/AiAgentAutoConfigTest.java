5de159b540557c4cdd1bb316b070fb99e63679e8
kiyasu <1207514710@qq.com>
2026-08-06 00:14:20 +0800

    feat: 2-11-ex 增加天气查询智能体

--- ai-agent-scaffold-app/src/test/java/com/kiyasu/ai/test/app/AiAgentAutoConfigTest.java
+++ ai-agent-scaffold-app/src/test/java/com/kiyasu/ai/test/app/AiAgentAutoConfigTest.java
@@ -51,4 +51,24 @@ public class AiAgentAutoConfigTest {
 
     }
 
+    @Test
+    public void weather_agent() throws InterruptedException {
+        AiAgentRegisterVO aiAgentRegisterVO = applicationContext.getBean("100002", AiAgentRegisterVO.class);
+
+        String appName = aiAgentRegisterVO.getAppName();
+        InMemoryRunner runner = aiAgentRegisterVO.getRunner();
+
+        Session session = runner.sessionService()
+                .createSession(appName, "kiyasu")
+                .blockingGet();
+
+        Content userMsg = Content.fromParts(Part.fromText("深圳明天会下雨吗"));
+        Flowable<Event> events = runner.runAsync("kiyasu", session.id(), userMsg);
+
+        List<String> outputs = new ArrayList<>();
+        events.blockingForEach(event -> outputs.add(event.stringifyContent()));
+
+        log.info("测试结果：{}", JSON.toJSONString(outputs));
+    }
+
 }
